2559788246ef01c20a7455e9d56c5cb76411a03a
Madli Reiljan <madlireiljan@gmail.com>
2024-09-18 10:56:17 +0300

    Ex 29

---
+++ src/people/Main.java
@@ -0,0 +1,21 @@
+package people;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    // Method to print all the people in the department
+    public static void printDepartment(List<Person> people) {
+        for (Person person : people) {
+            System.out.println(person);
+        }
+    }
+
+    public static void main(String[] args) {
+        List<Person> people = new ArrayList<>();
+        people.add(new Teacher("Pekka Mikkola", "Korsontie Street 1 03100 Vantaa", 1200));
+        people.add(new Student("Olli", "Ida Albergintie Street 1 00400 Helsinki"));
+
+        printDepartment(people);
+    }
+}
